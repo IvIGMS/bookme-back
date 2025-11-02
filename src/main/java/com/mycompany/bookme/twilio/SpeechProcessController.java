353593098feb10eb5868fef7bf767db116dccd32
Andrei Cimpoeru <acimpoeru@eservicios.indra.es>
2025-11-02 20:44:17 +0100

    Add openai dependency

--- src/main/java/com/mycompany/bookme/twilio/SpeechProcessController.java
+++ src/main/java/com/mycompany/bookme/twilio/SpeechProcessController.java
@@ -6,6 +6,7 @@ import com.twilio.twiml.VoiceResponse;
 import com.twilio.twiml.voice.Say;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.chat.client.ChatClient;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -21,6 +22,7 @@ import static java.util.Objects.isNull;
 public class SpeechProcessController {
 
     private final TwilioProperties twilioProperties;
+    private final ChatClient chatClient;
 
     @PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE, produces = MediaType.APPLICATION_XML_VALUE)
     public String processSpeechResult(
